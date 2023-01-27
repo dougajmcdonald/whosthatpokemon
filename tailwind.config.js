5f42da740375ad27e0b53dfcdce7f5415b98e926
Doug McDonald <dougajmcdonald@gmail.com>
2023-01-27 22:02:18 +0000

    Add POC API calls

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [    
+    './src/pages/**/*.{html,js}',
+    './src/components/**/*.{html,js}',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
