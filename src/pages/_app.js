1300fb61e6815a989cfc1645b07b43cf7416906d
Doug McDonald <dougajmcdonald@gmail.com>
2023-02-11 20:16:50 +0000

    fix + form

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,14 @@ import "@/styles/globals.css"
 import { GoogleAnalytics } from "nextjs-google-analytics"
 //import { SSRProvider } from "@next";
 
-export function reportWebVitals({ id, name, label, value }) {
-  event(name, {
-    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-    label: id, // id unique to current page load
-    nonInteraction: true, // avoids affecting bounce rate.
-  })
-}
+// export function reportWebVitals({ id, name, label, value }) {
+//   event(name, {
+//     category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+//     value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+//     label: id, // id unique to current page load
+//     nonInteraction: true, // avoids affecting bounce rate.
+//   })
+// }
 
 export default function App({ Component, pageProps }) {
   return (
