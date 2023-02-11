0fc6f664d12328f52393d654abee5bf72c5c68b5
Doug McDonald <dougajmcdonald@gmail.com>
2023-02-11 21:04:09 +0000

    rel

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,7 @@ export default function Home({ types, raidPokemon }) {
   return (
     <Layout title="Who's that Pokemon">
       <h1 className="text-4xl mb-8 text-center">Who&apos;s that Pokemon</h1>
-      <section className="mb-4 text-sm text-slate-400">
+      <section className="mb-4 text-sm text-slate-400 relative">
         <p className="inline-block">
           Who&apos;s that Pokemon helps your find the right pokemon for raids so
           you can spend your time battling, not reloading.
