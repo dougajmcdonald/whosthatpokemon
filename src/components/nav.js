b14efea5e8ff2e55cc083465c78882822d16dda4
Doug McDonald <dougajmcdonald@gmail.com>
2023-02-12 14:36:53 +0000

    add basic logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Link from "next/link"
+import Image from "next/image"
 
 const Nav = () => (
   <section className="flex flex-row h-16 bg-slate-800 w-full text-slate-50">
     <section className="flex items-center px-4 flex-grow">
-      Who&apos;s that Pokemon?
+      <Image
+        src="/img/logo v1.svg"
+        alt="Who's that Pokemon logo"
+        height="64"
+        width="122"
+      />
     </section>
     <nav className="flex flex-row">
       <Link
