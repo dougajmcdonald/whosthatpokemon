2af75cea0038263a23ad138b2490b47d9e833649
Doug McDonald <doug.mcdonald@moneyhub.com>
2023-02-15 08:56:00 +0000

    add 5*

--- src/data/raid_pokemon_5.js
+++ src/data/raid_pokemon_5.js
@@ -1,4 +1,4 @@
-export const raidPokemon6 = [
+export const raidpokemon6 = [
   { name: "raichu", moves: ["discharge", "iron-tail", "charm", "nuzzle"] },
   {
     name: "arcanine",
@@ -31,4 +31,336 @@ export const raidPokemon6 = [
     name: "dragonite",
     moves: ["dragon-rush", "aerial-ace", "extreme-speed", "hurricane"],
   },
-]
+  {
+    name: "slowking",
+    moves: ["psychic", "surf", "yawn", "water-pulse"],
+  },
+  {
+    name: "scizor",
+    moves: ["iron-head", "x-scissor", "bullet-punch", "slash"],
+  },
+  {
+    name: "delibird",
+    moves: ["present", "drill-peck", "ice-punch", "blizzard"],
+  },
+  {
+    name: "houndoom",
+    moves: ["flamethrower", "crunch", "taunt", "will-o-wisp"],
+  },
+  {
+    name: "blissey",
+    moves: ["dazzling-gleam", "hyper-voice", "sing", "seismic-toss"],
+  },
+  {
+    name: "tyranitar",
+    moves: ["rock-slide", "crunch", "screech", "dark-pulse"],
+  },
+  {
+    name: "gardevoir",
+    moves: ["psychic", "moonblast", "disable", "draining-kiss"],
+  },
+  {
+    name: "breloom",
+    moves: ["seed-bomb", "mach-punch", "worry-seed", "headbutt"],
+  },
+  {
+    name: "slaking",
+    moves: ["facade", "shadow-claw", "play-rough", "swagger"],
+  },
+  {
+    name: "hariyama",
+    moves: ["reversal", "brick-break", "brine", "heavy-slam"],
+  },
+  {
+    name: "sableye",
+    moves: ["shadow claw", "foul-play", "will-o-wisp", "night-shade"],
+  },
+  {
+    name: "camerupt",
+    moves: ["flamethrower", "earth-power", "yawn", "eruption"],
+  },
+  {
+    name: "altaria",
+    moves: ["dragon-pulse", "hurricane", "sing", "mist"],
+  },
+  {
+    name: "glalie",
+    moves: ["freeze-dry", "crunch", "headbutt", "frost breath"],
+  },
+  {
+    name: "salamence",
+    moves: ["dragon-rush", "aerial-ace", "hyper-voice", "draco-meteor"],
+  },
+  {
+    name: "staraptor",
+    moves: ["close-combat", "brave-bird", "quick-attack", "double-edge"],
+  },
+  {
+    name: "luxray",
+    moves: ["crunch", "wild-charge", "discharge", "thunder-wave"],
+  },
+  {
+    name: "drifblim",
+    moves: ["hex", "air-slash", "thunder-wave", "shadow-ball"],
+  },
+  {
+    name: "mismagius",
+    moves: ["mystical-fire", "shadow-ball", "confuse-ray", "taunt"],
+  },
+  {
+    name: "honchkrow",
+    moves: ["night-slash", "hurricane", "haze", "wing-attack"],
+  },
+  {
+    name: "bronzong",
+    moves: ["flash-cannon", "extrasensory", "metal-sound", "payback"],
+  },
+  {
+    name: "garchomp",
+    moves: ["earthquake", "dragon-claw", "iron-head", "slash"],
+  },
+  {
+    name: "hippowdon",
+    moves: ["earthquake", "yawn", "rock-slide", "body-slam"],
+  },
+  {
+    name: "abomasnow",
+    moves: ["energy-ball", "ice-punch", "ice-shard", "leer"],
+  },
+  {
+    name: "weavile",
+    moves: ["ice-punch", "night-slash", "taunt", "facade"],
+  },
+  {
+    name: "magnezone",
+    moves: ["thunderbolt", "flash-cannon", "tri-attack", "thunder-wave"],
+  },
+  {
+    name: "gallade",
+    moves: ["psycho-cut", "brick-break", "shadow-sneak", "fury-cutter"],
+  },
+  {
+    name: "froslass",
+    moves: ["frost-breath", "shadow-ball", "scary-face", "draining-kiss"],
+  },
+  {
+    name: "rotom",
+    moves: ["discharge", "uproar", "hex", "thunder-wave"],
+  },
+  {
+    name: "krookodile",
+    moves: ["earthquake", "crunch", "sand-tomb", "counter"],
+  },
+  {
+    name: "zoroark",
+    moves: ["night-daze", "shadow-claw", "taunt", "hyper-voice"],
+  },
+  {
+    name: "gothitelle",
+    moves: ["psychic", "thunder-wave", "thunderbolt", "stored-power"],
+  },
+  {
+    name: "amoonguss",
+    moves: ["energy-ball", "sludge-bomb", "spore", "clear-smog"],
+  },
+  {
+    name: "eelektross",
+    moves: ["wild-charge", "flamethrower", "discharge", "crush-claw"],
+  },
+  {
+    name: "haxorus",
+    moves: ["dragon-claw", "crunch", "giga-impact", "first-impression"],
+  },
+  {
+    name: "braviary",
+    moves: ["acrobatics", "crush-claw", "superpower", "air-slash"],
+  },
+  {
+    name: "hydreigon",
+    moves: ["dark-pulse", "dragon-pulse", "scary-face", "dragon-rush"],
+  },
+  {
+    name: "volcarona",
+    moves: ["fire-blast", "bug-buzz", "hurricane", "will-o-wisp"],
+  },
+  {
+    name: "talonflame",
+    moves: ["acrobatics", "flare-blitz", "steel-wing", "heat-wave"],
+  },
+  {
+    name: "florges",
+    moves: ["petal-dance", "moonblast", "psychic", "safeguard"],
+  },
+  {
+    name: "dragalge",
+    moves: ["dragon-pulse", "sludge-bomb", "water-pulse", "toxic"],
+  },
+  {
+    name: "clawitzer",
+    moves: ["water-pulse", "dragon-pulse", "aura-sphere", "crabhammer"],
+  },
+  {
+    name: "goodra",
+    moves: ["water=pulse", "dragon-pulse", "sludge-bomb", "power-whip"],
+  },
+  {
+    name: "avalugg",
+    moves: ["icicle-crash", "double-edge", "crunch", "ice-fang"],
+  },
+  {
+    name: "noivern",
+    moves: ["air-slash", "dragon-pulse", "acrobatics", "boomburst"],
+  },
+  {
+    name: "mudsdale",
+    moves: ["high-horsepower", "body-press", "rock-smash", "heavy-slam"],
+  },
+  {
+    name: "tsareena",
+    moves: ["high-jump-kick", "power-whip", "stomp", "trop-kick"],
+  },
+  {
+    name: "oranguru",
+    moves: ["facade", "psychic", "stored-power", "yawn"],
+  },
+  {
+    name: "passimian",
+    moves: ["reversal", "rock-smash", "facade", "gunk-shot"],
+  },
+  {
+    name: "mimikyu",
+    moves: ["play-rough", "shadow-claw", "will-o-wisp", "shadow-sneak"],
+  },
+  {
+    name: "greedent",
+    moves: ["body-slam", "body-press", "bullet-seed", "tail-whip"],
+  },
+  {
+    name: "corviknight",
+    moves: ["steel-wing", "drill-peck", "taunt", "body-press"],
+  },
+  {
+    name: "coalossal",
+    moves: ["heat-crash", "stone-edge", "incinerate", "ancient-power"],
+  },
+  {
+    name: "flapple",
+    moves: ["grav-apple", "dragon-breath", "dragon-rush", "trailblaze"],
+  },
+  {
+    name: "appletun",
+    moves: ["apple-acid", "dragon-pulse", "giga-drain", "body-press"],
+  },
+  {
+    name: "toxtricity",
+    moves: ["overdrive", "poison-jab", "nuzzle", "boomburst"],
+  },
+  {
+    name: "polteageist",
+    moves: ["shadow-ball", "mega-drain", "astonish", "will-o-wisp"],
+  },
+  {
+    name: "hatterene",
+    moves: ["dazzling-gleam", "psychic", "dark-pulse", "charm"],
+  },
+  {
+    name: "grimmsnarl",
+    moves: ["spirit-break", "false-surrender", "scary-face", "foul-play"],
+  },
+  {
+    name: "falinks",
+    moves: ["megahorn", "reversal", "headbutt", "brick-break"],
+  },
+  {
+    name: "pincurchin",
+    moves: ["zing-zap", "thunder", "surf", "poison-jab"],
+  },
+  {
+    name: "indeedee",
+    moves: ["psychic", "hyper-voice", "shadow-ball", "trick-room"],
+  },
+  {
+    name: "copperajah",
+    moves: ["heavy-slam", "strength", "curse", "high-horsepower"],
+  },
+  {
+    name: "dragapult",
+    moves: ["shadow-ball", "dragon-darts", "thunderbolt", "hex"],
+  },
+  {
+    name: "pawmot",
+    moves: ["wild-charge", "close-combat", "nuzzle", "sweet-kiss"],
+  },
+  {
+    name: "arboliva",
+    moves: ["energy-ball", "hyper-voice", "earth-power", "charm"],
+  },
+  {
+    name: "garganacl",
+    moves: ["salt-cure", "rock-slide", "hammer-arm", "sandstorm"],
+  },
+  {
+    name: "armarouge",
+    moves: ["armor-cannon", "psychic", "night-shade", "will-o-wisp"],
+  },
+  {
+    name: "ceruledge",
+    moves: ["bitter-blade", "shadow-claw", "psycho-cut", "will-o-wisp"],
+  },
+  {
+    name: "mabosstiff",
+    moves: ["crunch", "play-rough", "take-down", "swagger"],
+  },
+  {
+    name: "brambleghast",
+    moves: ["giga-drain", "shadow-ball", "power-whip", "infestation"],
+  },
+  {
+    name: "tinkaton",
+    moves: ["gigaton-hammer", "play-rough", "brutal-swing", "rock-smash"],
+  },
+  {
+    name: "bombirdier",
+    moves: ["rock-slide", "sucker-punch", "brave-bird", "torment"],
+  },
+  {
+    name: "palafin",
+    moves: ["liquidation", "acrobatics", "charm", "boomburst"],
+  },
+  {
+    name: "revavroom",
+    moves: ["spin-out", "taunt", "gunk-shot", "overheat"],
+  },
+  {
+    name: "orthworm",
+    moves: ["iron-head", "earthquake", "stomping-tantrum", "wrap"],
+  },
+  {
+    name: "glimmora",
+    moves: ["power-gem", "sludge-bomb", "mortal-spin", "ancient-power"],
+  },
+  {
+    name: "cetitan",
+    moves: ["ice-spinner", "liquidation", "yawn", "entrainment"],
+  },
+  {
+    name: "dondozo",
+    moves: ["order-up", "waterfall", "heavy-slam", "tickle"],
+  },
+  {
+    name: "tatsugiri",
+    moves: ["water-pulse", "dragon-pulse", "rapid-spin", "counter"],
+  },
+  {
+    name: "annihilape",
+    moves: ["shadow-claw", "close-combat", "outrage", "leer"],
+  },
+  {
+    name: "kingambit",
+    moves: ["iron-head", "night-slash", "torment", "slash"],
+  },
+  {
+    name: "baxcalibur",
+    moves: ["dragon-claw", "icicle-crash", "ice-shard", "body-press"],
+  },
+];
